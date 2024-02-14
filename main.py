be9cdd8eb844beb526f20db00a262aa4f3b8c9bd
CoekCx <nemanja.ja.jovicic7@gmail.com>
2024-02-14 16:37:10 +0100

    implemented fetching of top 1000 most popular books and using them as data

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 import os
 import re
 from collections import Counter
@@ -9,6 +10,12 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from tabulate import tabulate
 from tqdm import tqdm
+from prompt_toolkit import prompt
+from prompt_toolkit.completion import WordCompleter
+from prompt_toolkit.styles import Style
+
+
+# <editor-fold desc="Project Setup">
 
 
 # This should be run once before the first run of the script
@@ -17,21 +24,122 @@ def download_neccessary_nltk_data():
     nltk.download('stopwords')
 
 
-def fetch_moby_dick():
-    url = "https://www.gutenberg.org/files/2701/2701-h/2701-h.htm"  # URL of Moby Dick on Project Gutenberg
+# </editor-fold>
+
+# <editor-fold desc="Book Data Acquisition">
+
+def load_books_from_json():
+    if os.path.exists("book_data.json"):
+        with open("book_data.json", "r") as file:
+            books = json.load(file)
+        return books
+    else:
+        return None
+
+
+def save_books_to_json(books):
+    with open("book_data.json", "w") as file:
+        json.dump(books, file)
+
+
+def get_books_from_page(url):
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        book_links = soup.find_all('li', class_='booklink')
+
+        book_data = []
+        for link in book_links:
+            name = link.find('span', class_='title').text.strip()
+            relative_url = link.find('a')['href']
+            full_url = "https://www.gutenberg.org" + relative_url
+            book_data.append({'name': name, 'url': full_url})
+
+        return book_data
+    else:
+        print("Failed to fetch Gutenberg books:", response.status_code)
+        return None
+
+
+def get_all_gutenberg_books():
+    base_url = "https://www.gutenberg.org/ebooks/search/?start_index={}"
+    start_index = 1
+    all_books = []
+
+    os.system('cls' if os.name in ('nt', 'dos') else 'clear')
+    with tqdm(total=1000, desc="Fetching books", position=1, leave=True) as pbar:
+        while True:
+            url = base_url.format(start_index)
+            books_on_page = get_books_from_page(url)
+
+            if books_on_page:
+                all_books.extend(books_on_page)
+                start_index += 25
+                pbar.update(25)
+            else:
+                os.system('cls' if os.name in ('nt', 'dos') else 'clear')
+                print("Failed to fetch books. Exiting loop.")
+                break
+
+    return all_books
+
+
+def expand_books_with_or(names_and_urls):
+    expanded_books = []
+    for book in names_and_urls:
+        name = book['name']
+        url = book['url']
+
+        # Check if the name contains the pattern "{name1}; Or, {name2}"
+        if '; Or, ' in name:
+            name_parts = name.split('; Or, ')
+            for part in name_parts:
+                expanded_books.append({'name': part.strip(), 'url': url})
+        else:
+            expanded_books.append(book)
+    return expanded_books
+
+
+def prompt_user_for_book(books: list):
+    book_names = [book['name'] for book in books]
+
+    style = Style.from_dict({
+        'input': '#44ff44',
+        'placeholder': '#888888',
+    })
+
+    completer = WordCompleter(book_names, ignore_case=True, match_middle=True)
+
+    # Prompt text with a green background
+    prompt_text = [
+        ('class:prompt', 'Enter title: '),
+        ('class:input', ''),
+    ]
+
+    while True:
+        os.system('cls' if os.name in ('nt', 'dos') else 'clear')
+        input_book = prompt(prompt_text, style=style, completer=completer)
+        if input_book.strip() in book_names:
+            return input_book.strip()
+        else:
+            print("Please enter a valid book title.")
+
+
+# </editor-fold>
+
+# <editor-fold desc="Book Processing">
+
+
+def fetch_book(book_url: str):
+    url = f'{book_url}.txt.utf-8'
     response = requests.get(url)
 
     if response.status_code == 200:
         return response.text
     else:
-        print("Failed to fetch Moby Dick:", response.status_code)
+        print("Failed to fetch book:", response.status_code)
         return None
-
-
-def extract_text_from_html(html_content):
-    soup = BeautifulSoup(html_content, 'html.parser')
-    text = soup.get_text()
-    return text.strip()
 
 
 def extract_words(text):
@@ -59,7 +167,7 @@ def remove_stop_words(words, stop_words):
     return filtered_words
 
 
-def analyze_text(words):
+def analyze_text(book_name, words):
     total_words = len(words)
     unique_words = len(set(words))
     word_count = Counter(words).most_common(10)  # Top 10 most common words
@@ -69,21 +177,40 @@ def analyze_text(words):
 
     # Prepare data for tabular display
     table_data = [
-                     ["Total words in Moby Dick:", f"\033[95m{total_words}\033[0m"],
-                     ["Unique words in Moby Dick:", f"\033[95m{unique_words}\033[0m"],
+                     [f"Total words:", f"\033[95m{total_words}\033[0m"],
+                     [f"Unique words:", f"\033[95m{unique_words}\033[0m"],
                      ["", ""],  # Empty row for spacing
                      ["\033[96mTop 10 most common words:\033[0m", ""],
                  ] + colored_word_count
 
     # Print the table
     os.system('cls' if os.name in ('nt', 'dos') else 'clear')
-    print(tabulate(table_data, headers=["Word", "Count"], tablefmt="fancy_grid"))
+    print(tabulate(table_data, headers=[f"\033[96m{book_name}\033[0m", "Count"], tablefmt="fancy_grid"))
+
+
+# </editor-fold>
 
 
 def main():
+    # Load book data
+    books = load_books_from_json()
+    if not books:
+        books = get_all_gutenberg_books()  # Fetch book data online from Gutenberg
+        if not books:
+            os.system('cls' if os.name in ('nt', 'dos') else 'clear')
+            print("Failed to get the list of Gutenberg books.")
+            input()
+            return
+
+        books = expand_books_with_or(books)
+        save_books_to_json(books)
+
+    # Prompt user to select a book
+    selected_book_name = prompt_user_for_book(books)
+    selected_book_url = [x['url'] for x in books if x['name'] == selected_book_name][0]
+
     steps = [
-        'Fetch HTML content of Moby Dick',
-        'Extract text from HTML content',
+        'Fetch HTML (text) content of the book',
         'Extract words from text content',
         'Make words lowercase',
         'Remove non-words',
@@ -91,6 +218,7 @@ def main():
         'Remove stop words',
     ]
 
+    # Process book
     os.system('cls' if os.name in ('nt', 'dos') else 'clear')
     with tqdm(total=len(steps), desc="Processing steps", position=0, leave=True) as pbar:
         def take_next_step(next_step: str):
@@ -98,22 +226,13 @@ def main():
             pbar.set_postfix(ID=next_step)
 
         for index, step in enumerate(steps):
-            if step == 'Fetch HTML content of Moby Dick':
-                html_content = fetch_moby_dick()
-                if html_content:
-                    take_next_step(steps[index + 1])
-                else:
-                    os.system('cls' if os.name in ('nt', 'dos') else 'clear')
-                    print("Failed to fetch HTML content of Moby Dick.")
-                    return
-
-            elif step == 'Extract text from HTML content':
-                text_content = extract_text_from_html(html_content)
+            if step == 'Fetch HTML (text) content of the book':
+                text_content = fetch_book(selected_book_url)
                 if text_content:
                     take_next_step(steps[index + 1])
                 else:
                     os.system('cls' if os.name in ('nt', 'dos') else 'clear')
-                    print("Failed to extract text from HTML content.")
+                    print("Failed to fetch HTML content of Moby Dick.")
                     return
 
             elif step == 'Extract words from text content':
@@ -160,8 +279,8 @@ def main():
                     print("Failed to remove stop words.")
                     return
 
-    # Call analyze_text() to analyze the processed text
-    analyze_text(filtered_words)
+    # Analyze the processed text
+    analyze_text(selected_book_name, filtered_words)
 
 
 if __name__ == '__main__':
