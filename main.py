75d3418566437dd699369b57b2298ea7e46bd3e4
CoekCx <nemanja.ja.jovicic7@gmail.com>
2024-02-14 12:51:47 +0100

    implemented removing of non-words and words analysis

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import nltk
+import re
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from collections import Counter
@@ -39,6 +40,11 @@ def make_words_lowercase(words):
     return lowercase_words
 
 
+def remove_non_words(words):
+    cleaned_words = [word for word in words if re.match(r'^[a-zA-Z]+$', word)]
+    return cleaned_words
+
+
 def load_stop_words():
     stop_words = set(stopwords.words('english'))
     return stop_words
@@ -49,9 +55,17 @@ def remove_stop_words(words, stop_words):
     return filtered_words
 
 
-# TODO: Task 8: We have the answer
+def analyze_text(words):
+    total_words = len(words)
+    unique_words = len(set(words))
+    word_count = Counter(words).most_common(10)  # Top 10 most common words
 
-# TODO: Task 9: The most common word
+    print("Total words in Moby Dick:", total_words)
+    print("Unique words in Moby Dick:", unique_words)
+    print("\nTop 10 most common words:")
+    for word, count in word_count:
+        print(f"{word}: {count}")
+
 
 def main():
     # Fetch Moby Dick content
@@ -90,6 +104,15 @@ def main():
     else:
         print("Failed to convert words to lowercase.")
 
+    # Make words lowercase
+    filtered_lowercase_words = remove_non_words(lowercase_words)
+
+    # Check if words were converted to lowercase successfully
+    if filtered_lowercase_words:
+        print("Removed non words successfully!")
+    else:
+        print("Failed to removed non words.")
+
     # Load stop words
     stop_words = load_stop_words()
 
@@ -100,13 +123,16 @@ def main():
         print("Failed to load stop words.")
 
     # Call remove_stop_words() to filter out stop words
-    filtered_words = remove_stop_words(lowercase_words, stop_words)
+    filtered_words = remove_stop_words(filtered_lowercase_words, stop_words)
 
     # Check if stop words were removed successfully
     if filtered_words:
         print("Stop words removed successfully!")
     else:
         print("Failed to remove stop words.")
+
+    # Call analyze_text() to analyze the processed text
+    analyze_text(filtered_words)
 
 
 if __name__ == '__main__':
